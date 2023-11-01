409cf326cebcc7600e85dd914458a38676364219
dra2sun7 <dra2sun7@naver.com>
2023-11-01 14:10:30 +0900

    fix ver1.3

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/runCurl")
+        registry.addMapping("/**")
                 .allowedOrigins("http://www.kcs2.co.kr")
                 .allowCredentials(true);
     }
