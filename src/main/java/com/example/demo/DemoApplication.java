09079b56a70d6594bbf27d067529ad3050552a2f
ihyeonho <dra2sun7@gmail.com>
2025-03-16 22:27:57 +0900

    websocket test 1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-				SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
