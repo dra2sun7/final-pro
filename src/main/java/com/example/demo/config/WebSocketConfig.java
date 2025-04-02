67b7af70bcebf3dda4ce7913b8ece36e06f08732
ihyeonho <dra2sun7@gmail.com>
2025-04-02 17:45:56 +0900

    웹소켓 점검

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -4,8 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.sockjs.SockJsService;
 
 @Configuration
 @EnableWebSocket
@@ -15,7 +13,8 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         // WebSocket 핸들러 등록
         registry.addHandler(new WebSocketHandlerImpl(), "/ws") // WebSocket 엔드포인트를 "/ws"로 설정
-                .setAllowedOrigins("http://localhost:3000", "http://www.kcs2.co.kr") // 허용할 도메인 설정
-                .withSockJS(); // SockJS를 사용하여 WebSocket 연결을 지원
+                .setAllowedOrigins("*");
+//                .setAllowedOrigins("http://localhost:3000", "http://www.kcs2.co.kr") // 허용할 도메인 설정
+//                .withSockJS(); // SockJS를 사용하여 WebSocket 연결을 지원
     }
 }
--- src/main/java/com/example/demo/config/WebSocketHandlerImpl.java
+++ src/main/java/com/example/demo/config/WebSocketHandlerImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.config;
 
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -10,8 +9,9 @@ public class WebSocketHandlerImpl extends TextWebSocketHandler {
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
         // 클라이언트로부터 메시지를 받았을 때 처리하는 로직
+        System.out.prinltn("============================================================================================")
         System.out.println("Received message: " + message.getPayload());
-
+        System.out.prinltn("============================================================================================")
         // 클라이언트로 메시지 전송
         try {
             session.sendMessage(new TextMessage("Hello from server!"));
