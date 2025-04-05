e8067960d79dca0343351969cbb6cc14afa2ed02
ihyeonho <dra2sun7@gmail.com>
2025-04-05 18:33:19 +0900

    웹소켓 테스트

--- src/main/java/com/example/demo/config/WebSocketHandlerImpl.java
+++ src/main/java/com/example/demo/config/WebSocketHandlerImpl.java
@@ -9,12 +9,12 @@ public class WebSocketHandlerImpl extends TextWebSocketHandler {
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) {
         // 클라이언트로부터 메시지를 받았을 때 처리하는 로직
-        System.out.prinltn("============================================================================================")
+        System.out.println("============================================================================================");
         System.out.println("Received message: " + message.getPayload());
-        System.out.prinltn("============================================================================================")
+        System.out.println("============================================================================================");
         // 클라이언트로 메시지 전송
         try {
-            session.sendMessage(new TextMessage("Hello from server!"));
+            session.sendMessage(new TextMessage("서버에서 올바르게 전달함!"));
         } catch (Exception e) {
             e.printStackTrace();
         }
