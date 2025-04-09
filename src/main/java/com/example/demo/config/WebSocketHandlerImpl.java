53b9b64aed07720c76e25009acb2e6a0099339e1
ihyeonho <dra2sun7@gmail.com>
2025-04-09 17:30:35 +0900

    웹소켓 적용 및 메시지 박스 정렬

--- src/main/java/com/example/demo/config/WebSocketHandlerImpl.java
+++ src/main/java/com/example/demo/config/WebSocketHandlerImpl.java
@@ -1,22 +1,32 @@
 package com.example.demo.config;
 
-import org.springframework.web.socket.TextMessage;
-import org.springframework.web.socket.WebSocketSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.*;
+
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class WebSocketHandlerImpl extends TextWebSocketHandler {
 
+    private static final Logger log = LoggerFactory.getLogger(WebSocketHandlerImpl.class);
+
     @Override
-    public void handleTextMessage(WebSocketSession session, TextMessage message) {
-        // 클라이언트로부터 메시지를 받았을 때 처리하는 로직
-        System.out.println("============================================================================================");
-        System.out.println("Received message: " + message.getPayload());
-        System.out.println("============================================================================================");
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("WebSocket 연결 열림: {}", session.getId());
+    }
+
+    @Override
+    public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        log.info("============================================================================================");
+        log.info("Received message: {}", message.getPayload());
+        log.info("============================================================================================");
+
         // 클라이언트로 메시지 전송
-        try {
-            session.sendMessage(new TextMessage("서버에서 올바르게 전달함!"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        session.sendMessage(new TextMessage("서버에서 올바르게 전달함!"));
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        log.info("WebSocket 연결 종료됨: {}", session.getId());
     }
 }
